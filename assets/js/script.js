cfd89323d014a9f1a6dd125ab2bbe3fb699b0cab
carlopezzuti <carlopezzuti89@gmail.com>
2024-03-04 16:57:12 +0100

    add js bonus challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 const userName = prompt('Scrivi il tuo nome');
 const userSurname = prompt('Scrivi il tuo cognome');
 const userColour = prompt('Scrivi il tuo colore preferito');
+const fixedNumber = 23
+const userRandomNumber = prompt('Scrivi un numero tra 1 e 10')
+const userNumber = parseInt(userRandomNumber);
 
-let sum = userName + userSurname + userColour + '23';
+let sum = userNumber + fixedNumber;
 
-document.getElementById('message').innerHTML = sum;
+let userPassword = userName + userSurname + userColour + sum;
+
+document.getElementById('message').innerHTML = userPassword;
